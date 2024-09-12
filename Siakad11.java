32714552c149bafc7d61584de92ffeff08332b51
rumi <user@LAPTOP-0IU79HM9git config --global user.name rumigit onfig --get-all user.namegit onfig --get-all user.nameGH-Rumigit onfig --get-all user.name-- get-all GH-Rumi!git config --global user.email user@LAPTOP-0IU79HM9>
2024-09-12 09:52:37 +0700

    pembaruan

--- Siakad11.java
+++ Siakad11.java
@@ -3,25 +3,26 @@ public class Siakad11 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String nama, nim;
-        char kelas;
+        String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir;
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
         System.out.print("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen: ");
         absen = sc.nextByte();
         System.out.print("Masukkan nilai kuis: ");
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUts = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUas = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis*0.2) + (nilaiTugas*0.15) + (nilaiUts*0.3) + (nilaiUas*0.35);
         System.out.println("Nama: " + nama + " NIM: " + nim);
         System.out.println("Kelas: " + kelas + " Absen: " + absen);
         System.out.println("Nilai Akhir: " + nilaiAkhir);
